d7af968ecb26584c3932b630f7b9f3d0ac7817f4
Xiao Gui <xgui3783@gmail.com>
2020-05-18 12:23:09 +0200

    add netlify fn

---
+++ functions/datasetPreview/datasetPreview.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+
+exports.handler = (ev, ctx, cb) => {
+  const {
+    path,
+    httpMethod,
+    headers,
+    queryStringParameters,
+    body,
+    isBase64Encoded,
+  } = ev
+
+  // looks like one does not need filter by fn
+  //
+  // const {
+  //   filterBy
+  // } = queryStringParameters
+
+  // let parsedFilterBy
+  // try {
+  //   parsedFilterBy = filterBy && JSON.parse(filterBy)
+  // } catch (e) {
+  //   return cb(null, { status: 400 })
+  // }
+
+  const re0 = /datasetPreview\/(.+)/.exec(path)
+  if (!re0) {
+    return cb(null, {
+      status: 401
+    })
+  }
+  const fullReq = re0[1]
+  const re1 = /^([0-9a-f-]+)(\/.+)?$/.exec(fullReq)
+  if (!re1) {
+    return cb(null, {
+      status: 401
+    })
+  }
+  const datasetId = re1[1]
+  const filename = decodeURIComponent(re1[2].slice(1))
+
+  fs.readFile('./data/shapedPreviewData.json', 'utf-8', (err, data) => {
+    if (err) throw err
+    const arr = JSON.parse(data)
+
+    // filename not provided, return all files
+    const found = arr.find(([ dsId ]) => dsId === datasetId)
+    if (!found) {
+      return cb(null, { status: 404 })
+    }
+    const [ _, files ] = found
+
+    if (!filename) {
+      return cb(null, {
+        status: 200,
+        body: JSON.stringify(files),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+    }
+  
+    const foundFile = files.find(({ filename:fname }) => fname === filename)
+    if (!foundFile) {
+      return cb(null, { status: 404 })
+    }
+    // filename provided, return specified file
+    return cb(null, {
+      status: 200,
+      body: JSON.stringify(foundFile),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+  })
+}
